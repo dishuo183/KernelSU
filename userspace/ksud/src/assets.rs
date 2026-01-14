41e20178fae82af96873e73417c90d422e6cc67e
狄硕 <79615197+dishuo183@users.noreply.github.com>
2026-01-14 17:48:00 +0800

    Update assets.rs

--- userspace/ksud/src/assets.rs
+++ userspace/ksud/src/assets.rs
@@ -1,17 +1,11 @@
-use anyhow::Result;
-use std::path::Path;
-
 #[cfg(target_os = "android")]
 mod android {
     use crate::defs::BINARY_DIR;
-    use crate::utils::ensure_binary;
     use const_format::concatcp;
 
     pub const RESETPROP_PATH: &str = concatcp!(BINARY_DIR, "resetprop");
     pub const BUSYBOX_PATH: &str = concatcp!(BINARY_DIR, "busybox");
     pub const BOOTCTL_PATH: &str = concatcp!(BINARY_DIR, "bootctl");
-
-    // 不再需要ensure_binaries函数，因为不内置ko和ksuinit
 }
 
 #[cfg(target_os = "android")]
